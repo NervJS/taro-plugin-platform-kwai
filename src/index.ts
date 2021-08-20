fc702d6acd7c8eac1f4451a79ae534ff25410ee7
xuyan08 <xuyan08@kuaishou.com>
2021-08-20 11:52:59 +0800

    refactor(platform): 同步微信小程序插件-重构端平台插件运行时注入 API 的写法；同步优化小程序 API 的适配策略，只需要枚举异步 API; 修复 webpack 打包不同步 project.config 的问题

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ import type { IPluginContext } from '@tarojs/service'
 // 让其它平台插件可以继承此平台
 export { Kwai }
 
-export default (ctx: IPluginContext) => {
+export interface IOptions {
+  enablekeyboardAccessory: boolean
+}
+
+export default (ctx: IPluginContext, options: IOptions) => {
   ctx.registerPlatform({
     name: 'kwai',
     useConfigName: 'mini',
     async fn ({ config }) {
-      const program = new Kwai(ctx, config)
-      program.start()
+      const program = new Kwai(ctx, config, options || {})
+      await program.start()
     }
   })
 }
--- src/template.ts
+++ src/template.ts
@@ -1,6 +1,7 @@
-import { UnRecursiveTemplate } from '@tarojs/shared'
-
+import { UnRecursiveTemplate } from '@tarojs/shared/dist/template'
+import type { IOptions } from './index'
 export class Template extends UnRecursiveTemplate {
+  pluginOptions: IOptions
   supportXS = false
   Adapter = {
     if: 'ks:if',
@@ -11,5 +12,63 @@ export class Template extends UnRecursiveTemplate {
     forIndex: 'ks:for-index',
     key: 'ks:key',
     type: 'kwai'
+  }
+
+  constructor (pluginOptions: IOptions) {
+    super()
+    this.pluginOptions = pluginOptions || {}
+  }
+
+  replacePropName (name: string, value: string, componentName: string) {
+    if (value === 'eh') {
+      const nameLowerCase = name.toLowerCase()
+      if (nameLowerCase === 'bindlongtap' && componentName !== 'canvas') return 'bindlongpress'
+      return nameLowerCase
+    }
+    return name
+  }
+
+  buildXSTepFocus (nn: string) {
+    if (this.pluginOptions.enablekeyboardAccessory) {
+      return `function(i, prefix) {
+      var s = i.focus !== undefined ? 'focus' : 'blur'
+      var r = prefix + i.${nn} + '_' + s
+      if (i.nn === 'textarea' && i.cn[0] && i.cn[0].nn === 'keyboard-accessory') {
+        r = r + '_ka'
+      }
+      return r
+    }`
+    } else {
+      return super.buildXSTepFocus(nn)
+    }
+  }
+
+  modifyTemplateResult = (res: string, nodeName: string, _level, children) => {
+    if (nodeName === 'keyboard-accessory') return ''
+
+    if (nodeName === 'textarea' && this.pluginOptions.enablekeyboardAccessory) {
+      const list = res.split('</template>')
+
+      const target = `
+    <keyboard-accessory style="{{i.cn[0].st}}" class="{{i.cn[0].cl}}" bindtap="eh"  id="{{i.cn[0].uid}}">
+      <block wx:for="{{i.cn[0].cn}}" wx:key="uid">
+        <template is="{{xs.e(cid+1)}}" data="{{i:item,l:l}}" />
+      </block>
+    </keyboard-accessory>
+  `
+
+      const templateFocus = list[1]
+        .replace(children, target)
+        .replace('_textarea_focus', '_textarea_focus_ka')
+
+      const templateBlur = list[2]
+        .replace(children, target)
+        .replace('_textarea_blur', '_textarea_blur_ka')
+
+      list.splice(3, 0, templateFocus, templateBlur)
+      return list.join('</template>')
+    }
+
+    return res
   }
 }
