8cd95800c0e8dd37a8a7a264c9babc33566f71ea
xuyan08 <xuyan08@kuaishou.com>
2021-08-20 11:53:37 +0800

    feat: update types

--- types/runtime-utils.d.ts
+++ types/runtime-utils.d.ts
@@ -1,5 +1,6 @@
 import { initNativeApi } from './apis';
 export { initNativeApi };
+export * from './components';
 export * from './apis-list';
 export declare const hostConfig: {
     initNativeApi: typeof initNativeApi;
