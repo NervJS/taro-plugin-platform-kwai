cb0824117951d948b929b87bb1bdc0134b418a3c
赵雷涛 <zhaoleitao@le.com>
2021-08-24 20:49:42 +0800

    chore: 更新ignore

--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Kwai from './program';
-import type { IPluginContext } from '@tarojs/service';
-export { Kwai };
-declare const _default: (ctx: IPluginContext) => void;
-export default _default;
--- types/runtime-utils.d.ts
+++
@@ -1,6 +0,0 @@
-import { initNativeApi } from './apis';
-export { initNativeApi };
-export * from './apis-list';
-export declare const hostConfig: {
-    initNativeApi: typeof initNativeApi;
-};
