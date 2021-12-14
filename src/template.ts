a763f5bf02ad8c85b2bca7e937bf49c41f4d0d81
chenjiajian <798095202@qq.com>
2021-12-14 15:56:07 +0800

    fix(template): 优化文本节点模板

--- src/template.ts
+++ src/template.ts
@@ -1,8 +1,9 @@
-import { indent } from '@tarojs/shared'
+import { indent, Shortcuts } from '@tarojs/shared'
 import { RecursiveTemplate } from '@tarojs/shared/dist/template'
 
 export class Template extends RecursiveTemplate {
   flattenCoverViewLevel = 8
+  flattenTextLevel = 3
   supportXS = false
   Adapter = {
     if: 'ks:if',
@@ -42,9 +43,27 @@ export class Template extends RecursiveTemplate {
     return template
   }
 
+  buildFlattenText = (level = this.flattenTextLevel): string => {
+    if (level === 0) {
+      return `<block>{{i.${Shortcuts.Childnodes}[index].${Shortcuts.Text}}}</block>`
+    }
+
+    const child = this.buildFlattenText(level - 1)
+
+    const template =
+`<block ks:if="item.nn === '#text'">{{item.v}}</block><text ks:else id="{{item.uid}}" ${this.buildFlattenNodeAttributes('text')}>
+  <block ks:for="{{item.cn}}" ks:key="uid">
+    ${indent(child, 4)}
+  </block>
+</text>`
+    return template
+  }
+
   modifyLoopBody = (child: string, nodeName: string): string => {
     if (nodeName === 'cover-view') {
       return this.buildFlattenCoverView()
+    } else if (nodeName === 'static-text' || nodeName === 'text') {
+      return this.buildFlattenText()
     }
     return child
   }
