d6b786995c1f97f84c2d43ab1bfa1676bc4ab90e
mapleqin <qinmaples@gmail.com>
2022-02-20 01:18:54 +0800

    fix compile template data-sid property bug (#15)
    * fix compile template data-sid property bug
    
    * Completing the Button component callback event

--- src/components.ts
+++ src/components.ts
@@ -3,7 +3,12 @@ import { singleQuote } from '@tarojs/shared'
 export const components = {
   // ======== 调整属性 ========
   Button: {
-    bindGetPhoneNumber: ''
+    bindGetPhoneNumber: '',
+    bindGetUserInfo: '',
+    bindContact: '',
+    bindError: '',
+    bindOpenSetting: '',
+    bindLaunchApp: '',
   },
   Slider: {
     color: singleQuote('#e9e9e9'),
